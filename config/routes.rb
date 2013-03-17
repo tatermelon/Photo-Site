11cb9b3acc4e3a858966c5340696c70dfdd7037a
Tatiana Iskandar <tatermelon@gmail.com>
2013-03-16 20:16:54 -0700

    changed default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Project4::Application.routes.draw do
+  root :to => 'pics#users'
   match ':controller/:action/:id'
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -49,7 +50,7 @@ Project4::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   #root :to => 'pics#users'
 
   # See how all your routes lay out with "rake routes"
 
